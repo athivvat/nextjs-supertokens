bd7d64bac1715fcd9fe4391d8842150cbcab24e0
athivvat <athivvat@gmail.com>
2025-02-19 16:26:43 +0700

    Add /api/auth

--- app/config/appInfo.ts
+++ app/config/appInfo.ts
@@ -1,8 +1,11 @@
+export const websiteDomain = process.env.NEXT_PUBLIC_APP_DOMAIN ?? "http://localhost:3000";
+
 export const appInfo = {
     // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
     appName: "supertokens",
-    apiDomain: "http://localhost:3000",
-    websiteDomain: "http://localhost:3000",
-    apiBasePath: "/auth",
+    websiteDomain: websiteDomain,
+    websiteBasePath: "/auth",
+    apiDomain: websiteDomain,
+    apiBasePath: "/api/auth",
   }
   
--- app/config/backend.ts
+++ app/config/backend.ts
@@ -1,24 +1,26 @@
 import SuperTokens from "supertokens-node";
-import EmailPasswordNode from 'supertokens-node/recipe/emailpassword'
-import SessionNode from 'supertokens-node/recipe/session'
-import { appInfo } from './appInfo'
+import Dashboard from "supertokens-node/recipe/dashboard";
+import EmailPasswordNode from "supertokens-node/recipe/emailpassword";
+import SessionNode from "supertokens-node/recipe/session";
+import { appInfo } from "./appInfo";
 import { TypeInput } from "supertokens-node/types";
 
 export const backendConfig = (): TypeInput => {
   return {
     framework: "custom",
     supertokens: {
-      connectionURI: "<CORE_API_ENDPOINT>",
-      apiKey: "<YOUR_API_KEY>",
+      connectionURI: "http://localhost:3567",
+      // apiKey: "<YOUR_API_KEY>",
     },
     appInfo,
     recipeList: [
+      Dashboard.init(),
       EmailPasswordNode.init(),
       SessionNode.init(),
     ],
     isInServerlessEnv: true,
-  }
-}
+  };
+};
 
 let initialized = false;
 // This function is used in your APIs to make sure SuperTokens is initialised
